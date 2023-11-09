dedf040b4577c12b0995affed0eaac5417e990db
Lokesh-508 <lokeshsunkari508@gmail.com>
2023-11-09 22:41:53 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const app=express();
+const bodyParser =require('body-parser');
+const port =3000;
+
+
+ADMINS=[];
+USERS=[];
+app.use(bodyParser);
+
+
+app.post('/admin/signup',(req,res)=>{
+    const admin=req.body;
+    const existingAdmin=ADMINS.find(a=>a.username===admin.username);
+    if(existingAdmin){
+        res.status(403).json({message:'Admin already exists'});
+    }else{
+        ADMINS.push(admin);
+        res.json({message:'Admin created successfully'});
+    }
+});
+
+
+
+
+app.listen(port,()=>{
+      console.log('Server is listening on port 3000')
+});
